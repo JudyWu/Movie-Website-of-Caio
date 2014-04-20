ebb50c011751202305d9f9eea8b25b35246f2e8d
JudyWu <judyzhaoxinwu@gmail.com>
2014-04-20 18:01:42 -0400

    festival logos and various style tweaks

--- _site/myscript.js
+++ _site/myscript.js
@@ -1,45 +1,61 @@
 $(document).ready(function() {
 
-	$('#content').css('opacity', '0');
-	$('#content').css('display', 'block');
-	$('#content').hover(function() {
-		$('#content').stop().animate({opacity:1}, 'slow');
-	}, function() {
-		$('#content').stop().animate({opacity:0}, 'slow');
+	// $('#content').css('opacity', '0');
+	// $('#content').css('display', 'block');
+	// $('#content').hover(function() {
+	// 	$('#content').stop().animate({opacity:1}, 'slow');
+	// }, function() {
+	// 	$('#content').stop().animate({opacity:0}, 'slow');
 
-	});
+	// });
 
 	$('.my-sticky-element').waypoint('sticky');
 
 
 	$('.dropdownn-toggle').dropdown();
 
-
-	$('#sometime_in_august').waypoint(function() {
-		$('#sometime').toggleClass('highlight');
-		
+	$('.menu-item').each(function() {
+		var menuItem = $(this);
+		var menuItemId = '#link_to_' + menuItem.attr('id');
+		menuItem.waypoint(function() {
+			$('.highlight').removeClass('highlight');
+			$(menuItemId).addClass('highlight');
+		});
 	});
 
-    $('#letters_from_monte').waypoint(function() {
-		$('#letters').toggleClass('highlight');
-		
+	$('.remove-highlight').waypoint(function() {
+		$('.highlight').removeClass('highlight');
 	});
 
-	$('#two_lovers').waypoint(function() {
-		$('#lovers').toggleClass('highlight');
+	// $('#sometime_in_august').waypoint(function() {
+	// 	$('.highlight').removeClass('highlight');
+	// 	$('#link_to_sometime').addClass('highlight');
 		
-	});
+	// });
 
-	$('#the_realm').waypoint(function() {
-		$('#realm').toggleClass('highlight');
+ //    $('#letters_from_monte').waypoint(function() {
+ //    	$('.highlight').removeClass('highlight');
+	// 	$('#link_to_letters').addClass('highlight');
 		
-	});
+	// });
+
+	// $('#two_lovers').waypoint(function() {
+	// 	$('.highlight').removeClass('highlight');
+	// 	$('#link_to_lovers').addClass('highlight');
+		
+	// });
+
+	// $('#the_realm').waypoint(function() {
+	// 	$('.highlight').removeClass('highlight');
+	// 	$('#link_to_realm').addClass('highlight');
+		
+	// });
 
 
-    $('#sometime_in_august').waypoint(function() {
-        
-        $('#mid').toggleClass('showUp')
-    });
+    // $('#sometime_in_august').waypoint(function() {
+    //     // $('.highlight').removeClass('highlight');
+    //     $('#mid').addClass('showUp')
+    // });
 	
 
 });
