70bcb9bc1f40c179923b0d0e66647233e30a27e3
JudyWu <judyzhaoxinwu@gmail.com>
2014-04-21 11:20:37 -0400

    Make some comment hidden

--- myscript.js
+++ myscript.js
@@ -1,13 +1,5 @@
 $(document).ready(function() {
 
-	// $('#content').css('opacity', '0');
-	// $('#content').css('display', 'block');
-	// $('#content').hover(function() {
-	// 	$('#content').stop().animate({opacity:1}, 'slow');
-	// }, function() {
-	// 	$('#content').stop().animate({opacity:0}, 'slow');
-
-	// });
 
 	$('.my-sticky-element').waypoint('sticky');
 
@@ -26,6 +18,43 @@ $(document).ready(function() {
 	$('.remove-highlight').waypoint(function() {
 		$('.highlight').removeClass('highlight');
 	});
+     
+    $('#hidden_comment_biography').css('display', 'none');
+	$('#portrait_biograghy').hover(function() {
+        $('#hidden_comment_biography').animate({width: 'toggle'});
+     
+	});
+    
+
+    $('.hidden_comment').hide();
+    $('.the-realm-series').each(function() {
+    	
+    	var comment = $(this);
+    	var commentId = '#hidden_comment_' + comment.attr('id');
+    	
+    	comment.hover(function() {
+    		
+    		if($(commentId).is(':hidden')) {
+    			console.log("HIDDEN");
+    			$(commentId).slideDown('slow');
+    			$(commentId).css('visibility', 'visible');
+    		}
+    		else {
+    		
+    			$(commentId).slideUp();
+    		}
+    	});
+    });
+    
+
+	// $('#content').css('opacity', '0');
+	// $('#content').css('display', 'block');
+	// $('#content').hover(function() {
+	// 	$('#content').stop().animate({opacity:1}, 'slow');
+	// }, function() {
+	// 	$('#content').stop().animate({opacity:0}, 'slow');
+
+	// });
 
 	// $('#sometime_in_august').waypoint(function() {
 	// 	$('.highlight').removeClass('highlight');
