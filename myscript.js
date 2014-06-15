ed1d5bd9245e286cdb46356c0140de56643f5d4e
JudyWu <judyzhaoxinwu@gmail.com>
2014-06-15 12:44:09 -0400

    Changes to style

--- myscript.js
+++ myscript.js
@@ -6,87 +6,66 @@ $(document).ready(function() {
 
 	$('.my-sticky-element').waypoint('sticky');
 
+   
+    
 
-	$('.dropdownn-toggle').dropdown();
+    
+    //$('#myTab a:first').tab('show');
+
+ //    $('a[data-toggle="tab"]').on('shown.bs.tab', function (e) {
+	//   e.target // activated tab
+	//   e.relatedTarget // previous tab
+	// });
+
+    
+
+
+	//$('.dropdownn-toggle').dropdown();
 
 	$('.menu-item').each(function() {
 		var menuItem = $(this);
 		var menuItemId = '#link_to_' + menuItem.attr('id');
 		menuItem.waypoint(function() {
-			$('.highlight').removeClass('highlight');
-			$(menuItemId).addClass('highlight');
+            $('.highlight').removeClass('highlight');
+            $(menuItemId).toggleClass('highlight');
+            
+            // $('.highlight').removeClass('highlight');
+            // $(menuItemId).addClass('highlight');
+            
 		});
 	});
 
+    //$('.remove-highlight').removeClass('highlight');
+
+
+
 	
      
-    $('#hidden_comment_biography').css('display', 'none');
-	$('#portrait_biograghy').hover(function() {
-        $('#hidden_comment_biography').animate({width: 'toggle'});
+ //    $('#hidden_comment_biography').css('display', 'none');
+	// $('#portrait_biograghy').hover(function() {
+ //        $('#hidden_comment_biography').animate({width: 'toggle'});
      
-	});
+	// });
     
 
-    $('.hidden_comment').hide();
-    $('.the-realm-series').each(function() {
+    // $('.hidden_comment').hide();
+    // $('.the-realm-series').each(function() {
     	
-    	var comment = $(this);
-    	var commentId = '#hidden_comment_' + comment.attr('id');
+    // 	var comment = $(this);
+    // 	var commentId = '#hidden_comment_' + comment.attr('id');
     	
-    	comment.hover(function() {
+    // 	comment.hover(function() {
     		
-    		if($(commentId).is(':hidden')) {
-    			$(commentId).slideDown('slow');
-    			$(commentId).css('visibility', 'visible');
-    		}
-    		else {
+    // 		if($(commentId).is(':hidden')) {
+    // 			$(commentId).slideDown('slow');
+    // 			$(commentId).css('visibility', 'visible');
+    // 		}
+    // 		else {
     		
-    			$(commentId).slideUp();
-    		}
-    	});
-    });
-    
-
-	// $('#content').css('opacity', '0');
-	// $('#content').css('display', 'block');
-	// $('#content').hover(function() {
-	// 	$('#content').stop().animate({opacity:1}, 'slow');
-	// }, function() {
-	// 	$('#content').stop().animate({opacity:0}, 'slow');
-
-	// });
-
-	// $('#sometime_in_august').waypoint(function() {
-	// 	$('.highlight').removeClass('highlight');
-	// 	$('#link_to_sometime').addClass('highlight');
-		
-	// });
-
- //    $('#letters_from_monte').waypoint(function() {
- //    	$('.highlight').removeClass('highlight');
-	// 	$('#link_to_letters').addClass('highlight');
-		
-	// });
-
-	// $('#two_lovers').waypoint(function() {
-	// 	$('.highlight').removeClass('highlight');
-	// 	$('#link_to_lovers').addClass('highlight');
-		
-	// });
-
-	// $('#the_realm').waypoint(function() {
-	// 	$('.highlight').removeClass('highlight');
-	// 	$('#link_to_realm').addClass('highlight');
-		
-	// });
-
-
-    // $('#sometime_in_august').waypoint(function() {
-    //     // $('.highlight').removeClass('highlight');
-    //     $('#mid').addClass('showUp')
+    // 			$(commentId).slideUp();
+    // 		}
+    // 	});
     // });
-	
-
 });
 
 
